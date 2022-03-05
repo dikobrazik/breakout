ea677369cc64239d2e3039b10e0ca137e0085395
ilnarik <ilnarik@yandex-team.ru>
2022-03-05 10:56:38 +0300

    init project

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const { resolve } = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+const {BUILD} = process.env;
+
+module.exports = {
+  mode: BUILD ? 'production' : 'development',
+
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, 'dist'),
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+    static: './dist',
+    port: 1234,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?hash=sha512&digest=hex&name=images/[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optipng.optimizationLevel=7&gifsicle.interlaced=false',
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  plugins: [
+    new ProgressBarPlugin(),
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, `src/index.html`),
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+};
